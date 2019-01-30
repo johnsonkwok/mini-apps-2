c17c866ddc341cbf4bd65110f165ac89165d2a37
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-29 20:29:22 -0800

    Install modules and set up webpack

---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: __dirname + '/client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        }
+      }
+    ]
+  }
+};
