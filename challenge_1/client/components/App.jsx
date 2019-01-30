ebf76ad325ab1d2fc311e23005dcf4b63cb3d31f
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-30 14:38:22 -0800

    Style pagination bar with Bootstrap
    * Change page numbering to begin with 1 instead of 0

--- challenge_1/client/components/App.jsx
+++ challenge_1/client/components/App.jsx
@@ -2,24 +2,27 @@ import React from 'react';
 import Search from './Search.jsx';
 import EventList from './EventList.jsx';
 import ReactPaginate from 'react-paginate';
+import axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       events: [],
-      pageNum: 0,
+      pageNum: 1,
+      pageCount: 1,
       query: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handlePageClick = this.handlePageClick.bind(this);
   }
 
-  loadEventsFromServer(query = '', page = 1) {
-    fetch(`/events?q=${query}&_limit=10&_page=${this.state.pageNum}`)
-      .then(res => res.json())
-      .then((events) => {
-        this.setState({ events });
+  loadEventsFromServer(query = '') {
+    axios.get(`/events?q=${query}&_limit=10&_page=${this.state.pageNum}`)
+      .then(res => {
+        const pageCount = Math.ceil(res.headers['x-total-count'] / 10);
+        const events = res.data;
+        this.setState({ events, pageCount });
       })
       .catch(err => console.error(err));
   }
@@ -34,7 +37,8 @@ class App extends React.Component {
   }
 
   handlePageClick(data) {
-    let pageNum = data.selected;
+    let pageNum = data.selected + 1;
+    console.log('page number', pageNum);
     this.setState({ pageNum }, () => {
       this.loadEventsFromServer();
     });
@@ -45,23 +49,24 @@ class App extends React.Component {
       <div>
         <Search />
         <EventList events={this.state.events} />
-        <ReactPaginate
-          previousLabel={'previous'}
-          nextLabel={'next'}
-          breakLabel={'...'}
-          breakClassName={'break-me'}
-
-          //  Total page count (with 10 items per page displayed) obtained by making GET req 
-          // to '/events?_start=1&_limit=10' and checking the amount in the 'X-Total-Count'
-          // header, then dividing that number by 10
-          pageCount={3786}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={this.handlePageClick}
-          containerClassName={'pagination'}
-          subContainerClassName={'pages pagination'}
-          activeClassName={'active'}
-        />
+        <nav id="react-paginate">
+          <ReactPaginate
+            previousLabel={'previous'}
+            nextLabel={'next'}
+            breakLabel={'...'}
+            pageCount={this.state.pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={this.handlePageClick}
+            containerClassName={'pagination'}
+            pageClassName={'page-item'}
+            pageLinkClassName={'page-link'}
+            previousLinkClassName={'page-link'}
+            nextLinkClassName={'page-link'}
+            breakLinkClassName={'page-link'}
+            activeClassName={'active'}
+          />
+        </nav>
       </div>
     );
   }
