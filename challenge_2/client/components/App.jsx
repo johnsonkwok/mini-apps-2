9f3b5a43c72fba88da2bf5b8760d20f6a480d38a
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-31 13:42:35 -0800

    Retrieve data from server when App component mounts

--- challenge_2/client/components/App.jsx
+++ challenge_2/client/components/App.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
+import Chart from './components/Chart';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      chartData: {}
     };
+  }
+
+  componentDidMount() {
+    fetch('/bpi')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ data });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 
   render() {
     return (
       <div>
-        Hello Worlds
+        <Chart chartData={this.state.chartData} />
       </div>
     );
   }
