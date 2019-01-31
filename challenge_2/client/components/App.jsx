b4989e77d49a37867045517b37b43d19e823acea
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-31 12:42:53 -0800

    Implement request to CoinDesk Bitcoin Price Index API
    * Add Chart component

--- challenge_2/client/components/App.jsx
+++ challenge_2/client/components/App.jsx
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+
+    };
   }
 
   render() {
---
+++ challenge_2/client/components/Chart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Line} from 'react-chartjs-2';
+
+const Chart = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Chart;
