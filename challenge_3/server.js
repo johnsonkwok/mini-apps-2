9e2c69cbe3014f20f69f99064c924e5b1a080349
Johnson Kwok <johnsonkwok1@gmail.com>
2019-01-31 21:23:13 -0800

    Challenge 3: Set up server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('tiny'));
 
 
+
+const port = process.env.port || 3000;
+app.listen(port, () => console.log(`App listening on port ${port}`));
